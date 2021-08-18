f266c71fb40ac4aafe07979408122a774ee11b75
Prabhjot <pjsingh1359@gmail.com>
2021-08-18 07:31:58 -0700

    working create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 3, maximum: 20 }, uniqe: true
+  validates :username, presence: true, length: { minimum: 3, maximum: 20 }
   validates :email, presence: true, length: { minimum: 3, maximum: 100 }
   validates :password, presence: true, length: { minimum: 6, maximum: 20 }
 end
